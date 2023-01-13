c9f38a61a7035b40ec57c9f99a13b9ae9ce2ea92
Emmanuel Itakpe <62019510+Emmanuel-Develops@users.noreply.github.com>
2023-01-13 21:50:31 +0100

    closes #13, Mapping links to name (#16)
    * Mapping links to name
    * Added EOF line, prettier config to ensure code formatting consistency

---
+++ src/components/CustomMultiCheckboxFacet.js
@@ -0,0 +1,116 @@
+import React from "react";
+// import { FacetViewProps } from "./types";
+// import type { FieldValue } from "@elastic/search-ui";
+import mapping from "../config/mapping.json";
+
+function CustomMultiCheckboxFacet({
+  className,
+  label,
+  onMoreClick,
+  onRemove,
+  onSelect,
+  options,
+  showMore,
+  showSearch,
+  onSearch,
+  searchPlaceholder,
+}) {
+  // This function was modified to add the mapping of names to links using mapping?.labels[filterValue]
+  function getFilterValueDisplay(filterValue) {
+    if (filterValue === undefined || filterValue === null) {
+      return "";
+    }
+    if (Object.prototype.hasOwnProperty.call(filterValue, "name")) {
+      return filterValue.name;
+    }
+    if (mapping?.labels[filterValue]) {
+      return mapping?.labels[filterValue];
+    }
+    return String(filterValue);
+  }
+
+  function getNewClassName(newClassName) {
+    if (!Array.isArray(newClassName)) return newClassName;
+    return newClassName.filter((name) => name).join(" ");
+  }
+  function appendClassName(baseClassName, newClassName) {
+    if (!newClassName) {
+      return (
+        (Array.isArray(baseClassName)
+          ? baseClassName.join(" ")
+          : baseClassName) || ""
+      );
+    }
+    if (!baseClassName) {
+      return getNewClassName(newClassName) || "";
+    }
+    return `${baseClassName} ${getNewClassName(newClassName)}`;
+  }
+  return (
+    <fieldset className={appendClassName("sui-facet", className)}>
+      <legend className="sui-facet__title">{label}</legend>
+
+      {showSearch && (
+        <div className="sui-facet-search">
+          <input
+            className="sui-facet-search__text-input"
+            type="search"
+            placeholder={searchPlaceholder || "Search"}
+            onChange={(e) => {
+              onSearch(e.target.value);
+            }}
+          />
+        </div>
+      )}
+
+      <div className="sui-multi-checkbox-facet">
+        {options.length < 1 && <div>No matching options</div>}
+        {options?.map((option) => {
+          const checked = option.selected;
+          const value = option.value;
+          return (
+            <label
+              key={`${getFilterValueDisplay(option.value)}`}
+              htmlFor={`example_facet_${label}${getFilterValueDisplay(
+                option.value
+              )}`}
+              className="sui-multi-checkbox-facet__option-label"
+            >
+              <div className="sui-multi-checkbox-facet__option-input-wrapper">
+                <input
+                  data-transaction-name={`facet - ${label}`}
+                  id={`example_facet_${label}${getFilterValueDisplay(
+                    option.value
+                  )}`}
+                  type="checkbox"
+                  className="sui-multi-checkbox-facet__checkbox"
+                  checked={checked}
+                  onChange={() => (checked ? onRemove(value) : onSelect(value))}
+                />
+                <span className="sui-multi-checkbox-facet__input-text">
+                  {getFilterValueDisplay(option.value)}
+                </span>
+              </div>
+              <span className="sui-multi-checkbox-facet__option-count">
+                {option.count.toLocaleString("en")}
+              </span>
+            </label>
+          );
+        })}
+      </div>
+
+      {showMore && (
+        <button
+          type="button"
+          className="sui-facet-view-more"
+          onClick={onMoreClick}
+          aria-label="Show more options"
+        >
+          + More
+        </button>
+      )}
+    </fieldset>
+  );
+}
+
+export default CustomMultiCheckboxFacet;
