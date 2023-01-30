b067c00336fdd2558dca3e8d081dc61ba7846f55
Emmanuel-Develops <itakpeemma@gmail.com>
2023-01-30 18:34:02 +0100

    refactor: segregated styles

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import {
 } from "@elastic/react-search-ui";
 import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
-import "./custom.css";
+import "./custom.scss";
 
 import {
   buildAutocompleteQueryConfig,
