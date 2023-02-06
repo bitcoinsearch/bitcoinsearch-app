0aeef18d1d0285ba3d9d7b051362e089fbb87df2
Emmanuel Itakpe <62019510+Emmanuel-Develops@users.noreply.github.com>
2023-02-06 13:55:52 +0100

    Mailing list grouping (#46)

--- src/components/customResults/CustomResults.jsx
+++ src/components/customResults/CustomResults.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { withSearch } from "@elastic/react-search-ui";
 import ResultCollection from "./ResultCollection";
-import { getDomainGrouping, getDomainLabel } from "../../config/mapping-helper";
+import { getDomainGrouping } from "../../config/mapping-helper";
 import "./styles.results.scss";
+import {
+  generateLocator,
+  sortGroupedResults,
+} from "../../config/results-helper";
 
 const CustomResults = ({
   results,
@@ -16,25 +20,35 @@ const CustomResults = ({
 
   const formattedResults = [];
   const similarity = {};
+  const groupedIndices = new Set();
   const groupedDomains = getDomainGrouping();
+
   results.forEach((result) => {
-    if (groupedDomains.includes(result.domain.raw)) {
+    const raw_domain = result?.domain?.raw ?? null;
+
+    if (groupedDomains.includes(raw_domain)) {
       const idx = formattedResults.length;
-      const label = getDomainLabel(result.domain.raw, true);
-      const locatorString = label
-        ? `${result.title.raw}_${label}`
-        : result.title.raw;
-      const isSimilarIdx = similarity[locatorString];
+
+      const locatorId = generateLocator(
+        raw_domain,
+        result.url.raw,
+        result.title.raw
+      );
+      const isSimilarIdx = similarity[locatorId];
+
       if (isSimilarIdx !== undefined) {
         formattedResults[isSimilarIdx].push({ ...result });
+        groupedIndices.add(isSimilarIdx);
       } else {
         formattedResults.push([{ ...result }]);
-        similarity[locatorString] = idx;
+        similarity[locatorId] = idx;
       }
     } else {
       formattedResults.push([{ ...result }]);
     }
   });
+
+  sortGroupedResults(groupedIndices, formattedResults);
 
   const resultProps = {
     clickThroughTags,
