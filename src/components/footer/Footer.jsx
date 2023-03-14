35af01d4d27c869861bd71819084a73a559ef1dc
balogunofafrica <balogunakanbi.k@gmail.com>
2023-03-14 10:03:01 +0100

    feat: add link to chaincode

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,26 @@
 import { Paging } from "@elastic/react-search-ui";
 import React from "react";
 import useIsInitialStateWithoutFilter from "../../hooks/useIsInitialStateWithoutFilter";
+import "./footer.scss";
 
 const Footer = () => {
   const { hiddenBody } = useIsInitialStateWithoutFilter();
   if (hiddenBody) {
     return null;
   }
-  return <Paging />;
+  return (
+    <div className="footer-container">
+      <Paging />
+      <a
+        href="https://chaincode.com"
+        target="_blank"
+        rel="noreferrer"
+        className="link"
+      >
+        ❤️ Chaincode
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
