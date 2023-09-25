74421b3c1fdddedd747474844e928932adb52c96
Emmanuel-Develops <itakpeemma@gmail.com>
2023-09-25 02:44:27 +0100

    refactor: move client to root directory

--- backend/core/routes/v1/search.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const search = require('../../controllers/search');
-
-router.post('/search', search.search);
-
-module.exports = router;
