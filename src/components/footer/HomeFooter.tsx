c2f1b8fc625290d4caf664d9a3471b637864b70a
balogunofafrica <balogunakanbi.k@gmail.com>
2024-03-12 09:28:05 +0100

    add footer

--- src/components/footer/HomeFooter.tsx
+++ src/components/footer/HomeFooter.tsx
@@ -1,35 +1,53 @@
-import React from "react";
-import useIsInitialStateWithoutFilter from "../../hooks/useIsInitialStateWithoutFilter";
+import GithubIcon from "../svgs/GithubIcon";
+
+const Separator = ({ className }: { className?: string }) => (
+  <div className={`h-6 border-2 border-[#BFBFBF] ${className}`}></div>
+);
 
 const HomeFooter = () => {
-  const { hiddenBody } = useIsInitialStateWithoutFilter();
-  if (!hiddenBody) {
-    return null;
-  }
   return (
-    <div className="home-footer">
-      <a
-        href="https://bitcoindevs.xyz/"
-        target="_blank"
-        rel="noreferrer"
-        className="chaincode-link"
-      >
-        ❤️ Bitcoin Dev Project
-      </a>
-
-      <div className="umami">
-        <p>
-          Visitor counts publicly available via{" "}
-        <a
-          href="https://visits.bitcoindevs.xyz/share/0Beh7BUzocqrtgA5/bitcoin-search"
-          target="_blank"
-          rel="noreferrer"
-        >
-          umami
+    <footer className="bg-white lg:text-left fixed bottom-0 gap-7 text-center flex flex-col md:flex-row items-center justify-center p-11 text-black w-full border-t-[1px] border-[#BFBFBF]">
+      <div className="flex gap-7 items-center">
+        <button className="text-xs font-semibold text-white focus:outline-none py-4 px-6 bg-[#292929] rounded-xl">
+          Give feedback
+        </button>
+        <Separator />
+        <div className="flex-grow text-xs text-gray-700">
+          Built with <span className="text-red-500">🧡</span> by the{" "}
+          <a href="#" className="underline font-medium text-[#EC802F]">
+            Bitcoin Dev Project
+          </a>
+        </div>
+        <Separator />
+        <a className="text-xs text-gray-700 hover:underline focus:outline-none">
+          View our public visitor counts
         </a>
-        </p>
       </div>
-    </div>
+      <Separator className="hidden md:block" />
+      <div className="flex gap-7 items-center">
+        <div className="flex-grow text-xs text-gray-700">
+          <p>Stay in touch with the Bitcoin Dev Project</p>
+          (Please consider using an email alias.{" "}
+          <a href="#" className="hover:underline">
+            Here&apos;s how.
+          </a>
+          )
+        </div>
+        <div className="flex items-center gap-2">
+          <input
+            type="text"
+            placeholder="you@email.com"
+            className="text-xs text-gray-700 p-4 rounded-xl border border-gray-300 h-full focus:outline-none focus:border-gray-500"
+          />
+          <button className="text-xs font-semibold text-white focus:outline-none py-4 px-6 bg-[#292929] rounded-xl">
+            Keep me updated
+          </button>
+        </div>
+        <button className="focus:outline-none">
+          <GithubIcon />
+        </button>
+      </div>
+    </footer>
   );
 };
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import HomeFooter from "@/components/footer/HomeFooter";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head />
       <body>
         <Main />
         <NextScript />
+        <HomeFooter />
       </body>
     </Html>
-  )
+  );
 }
