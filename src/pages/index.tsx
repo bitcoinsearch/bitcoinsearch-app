d12da127a52876eacd20c6a4b96263adf27851c5
balogunofafrica <balogunakanbi.k@gmail.com>
2024-03-21 10:37:31 +0100

    refactor: home text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import FormModal from "@/components/formModal/FormModal";
 import LoadingBar from "@/components/loadingBar/LoadingBar";
 import NoResults from "@/components/noResultsCard/NoResults";
 import { useSearchFocusHotkey } from "@/hooks/useGlobalHotkey";
+import useIsInitialStateWithoutFilter from "@/hooks/useIsInitialStateWithoutFilter";
 import useScrollTop from "@/hooks/useScrollTop";
 import useSearchQuery from "@/hooks/useSearchQuery";
 import Header from "@/layout/Header";
@@ -19,7 +20,33 @@ import Image from "next/image";
 import useUIContext from "@/hooks/useUIContext";
 import { LandingPage } from "@/components/landingPage/LandingPage";
 import { useRouter } from "next/router";
-import useIsInitialStateWithoutFilter from "@/hooks/useIsInitialStateWithoutFilter";
+
+const HomeTextBanner = () => {
+  const { hiddenHomeFacet } = useIsInitialStateWithoutFilter();
+
+  if (hiddenHomeFacet) return null;
+
+  return (
+    <div className="flex flex-col gap-4 text-center mb-auto pt-[8%] pb-4">
+      <div className="flex gap-2 md:gap-3 items-center self-center">
+        <Image
+          src="/btc.png"
+          className="w-8 h-8 md:w-16 md:h-16"
+          alt="bitcoin logo"
+          width={36}
+          height={36}
+          priority
+        />
+        <p className="text-3xl md:text-6xl font-bold italic text-gray bg-gradient-92 from-brightOrange-100 to-brightOrange-300 text-opacity-0 bg-clip-text p-1">
+          bitcoin search
+        </p>
+      </div>
+      <p className="text-sm md:text-2xl text-darkGray-200">
+        Search the depths of bitcoin&apos;s technical ecosystem
+      </p>
+    </div>
+  );
+};
 
 export default function App() {
   useSearchFocusHotkey();
@@ -31,32 +58,41 @@ export default function App() {
   const router = useRouter();
 
   // INFERENCES
-  const hasFilters = Boolean(generateFilterQuery(router.asPath.slice(1)).length);
+  const hasFilters = Boolean(
+    generateFilterQuery(router.asPath.slice(1)).length
+  );
   const hasQueryString = Boolean(searchQuery?.trim());
   const isLoading = queryResult.isFetching;
-  const noResult = (hasFilters || hasQueryString) && !isLoading && !queryResult.data?.hits?.total["value"];
+  const noResult =
+    (hasFilters || hasQueryString) &&
+    !isLoading &&
+    !queryResult.data?.hits?.total["value"];
   const results = queryResult.data?.hits?.hits ?? [];
   console.log({ results });
 
   return (
     <div className={`${isHomePage && "relative"}`}>
-      <main className='min-h-[95vh] flex w-full items-center bg-white'>
+      <main className="min-h-[95vh] flex w-full items-center bg-white">
         {isLoading && <LoadingBar />}
-        <div className={`App btc-search w-full ${!results.length && !noResult && "-mt-40"}`}>
-          <div className={`header ${results.length && 'pt-10'}`}>
+        <div
+          className={`App btc-search w-full ${
+            !results.length && !noResult && "-mt-40"
+          }`}
+        >
+          <div className={`header ${results.length && "pt-10"}`}>
             <Image
-              src='/btc-main.png'
-              className='logo mx-auto max-w-[200px] md:max-w-xs lg:max-w-lg 2xl:max-w-xl'
-              alt='bitcoin logo'
+              src="/btc-main.png"
+              className="logo mx-auto max-w-[200px] md:max-w-xs lg:max-w-lg 2xl:max-w-xl"
+              alt="bitcoin logo"
               width={459}
               height={69}
               priority
             />
-            <p className='text-custom-black-dark leading-normal text-sm  sm:text-base lg:text-2xl xl:text-2xl'>
+            <p className="text-custom-black-dark leading-normal text-sm  sm:text-base lg:text-2xl xl:text-2xl">
               Search the depths of bitcoin’s technical ecosystem
             </p>
           </div>
-          <div className='pt-12'></div>
+          <div className="pt-12"></div>
           <Layout
             header={<Header openForm={openForm} />}
             sideContent={<SideBar />}
@@ -69,7 +105,9 @@ export default function App() {
         </div>
       </main>
       {NoResults && isHomePage && <LandingPage />}
-      <section className={`${(isLoading || !hasQueryString || noResult) && "hidden"}`}>
+      <section
+        className={`${(isLoading || !hasQueryString || noResult) && "hidden"}`}
+      >
         <ResultFooter />
       </section>
     </div>
