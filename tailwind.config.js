e6ed9da83520eec54b9e91495732ef218c6ccc2f
IgboPharaoh <solomoneze62@gmail.com>
2024-03-27 08:41:35 +0100

    add necessary assets and style configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "secondary-black": '#292929',
+        "tertiary-black": '#4d4d4d',
+        "primary-orange": '#F7931A'
+      }
     },
   },
   plugins: [],
