83180e4f4c34a7b118638cca82cb48674932da96
balogunofafrica <balogunakanbi.k@gmail.com>
2024-03-29 09:26:06 +0100

    refactor: app item

--- src/components/appMenu/index.tsx
+++ src/components/appMenu/index.tsx
@@ -3,7 +3,7 @@ import { menuApps } from "./data";
 
 export function AppMenu() {
   return (
-    <div className="ml-10 mb-[1000px] py-5 rounded-2xl border border-gray w-[402px]">
+    <div className="ml-10 mb-[1000px] bg-white py-5 rounded-2xl border border-gray w-[402px] max-w-xs xl:max-w-[402px] max-h-[70vh] overflow-scroll">
       <AppItem {...menuApps[0]} />
       <div className="mx-7 my-3 border border-gray"></div>
       {menuApps.slice(1).map((item) => (
