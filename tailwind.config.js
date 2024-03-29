d5d510551edaa3474aff633b6b963a573d890453
balogunofafrica <balogunakanbi.k@gmail.com>
2024-03-29 17:38:44 +0100

    fix: remove used gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,6 @@ module.exports = {
         },
       },
       backgroundImage: {
-        "gradient-92": "linear-gradient(92deg, var(--tw-gradient-stops))",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
