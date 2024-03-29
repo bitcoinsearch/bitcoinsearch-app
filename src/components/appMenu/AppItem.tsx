735e242311a294d6eb50d96fd2a7adac756af076
balogunofafrica <balogunakanbi.k@gmail.com>
2024-03-29 16:37:03 +0100

    fix: apps data link

--- src/components/appMenu/AppItem.tsx
+++ src/components/appMenu/AppItem.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import type { MenuApp } from "./data";
+import Link from "next/link";
 
 export const AppItem = ({ href, image, name, title }: MenuApp) => (
-  <a href={href} className="py-3 px-8 gap-6 flex items-center">
+  <Link
+    href={href}
+    className="py-3 px-8 gap-6 flex items-center"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
     <Image
       className="rounded-xl w-11 h-11 lg:w-16 lg:h-16 xl:w-20 xl:h-20"
       src={image}
@@ -13,5 +19,5 @@ export const AppItem = ({ href, image, name, title }: MenuApp) => (
     <p className="text-xs md:text-sm xl:text-base 2xl:text-lg text-left text-custom-lightGrey">
       {title}
     </p>
-  </a>
+  </Link>
 );
