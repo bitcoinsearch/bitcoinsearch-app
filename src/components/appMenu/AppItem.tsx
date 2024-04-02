9b57af64c419abcbc84b10f6b11dd91cd6c166e6
balogunofafrica <balogunakanbi.k@gmail.com>
2024-04-02 20:44:30 +0100

    fix app item bg

--- src/components/appMenu/AppItem.tsx
+++ src/components/appMenu/AppItem.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export const AppItem = ({ href, image, name, title }: MenuApp) => (
   <Link
     href={href}
-    className="py-3 px-8 gap-6 flex items-center hover:bg-custom-stroke"
+    className="py-3 px-8 gap-6 flex items-center hover:bg-custom-hover-state dark:hover:bg-custom-stroke"
     target="_blank"
     rel="noopener noreferrer"
   >
--- src/components/appMenu/index.tsx
+++ src/components/appMenu/index.tsx
@@ -3,7 +3,7 @@ import { menuApps } from "./data";
 
 export function AppMenu() {
   return (
-    <div className="ml-10 mb-[1000px] bg-custom-hover-state py-5 rounded-2xl border border-custom-stroke w-[402px] max-w-xs xl:max-w-[402px] max-h-[70vh] overflow-scroll">
+    <div className="ml-10 mb-[1000px] bg-custom-background dark:bg-custom-hover-state py-5 rounded-2xl border border-custom-stroke w-[402px] max-w-xs xl:max-w-[402px] max-h-[70vh] overflow-scroll">
       <AppItem {...menuApps[0]} />
       <div className="mx-7 my-3 border border-custom-stroke"></div>
       {menuApps.slice(1).map((item) => (
