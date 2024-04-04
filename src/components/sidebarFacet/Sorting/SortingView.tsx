152f1373d630f9b9fe55b404ca4a2f5e5531c028
Emmanuel-Develops <itakpeemma@gmail.com>
2024-04-04 11:44:22 +0100

    fix: sort styling

--- src/components/sidebarFacet/Sorting/SortingView.tsx
+++ src/components/sidebarFacet/Sorting/SortingView.tsx
@@ -4,7 +4,6 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
-  useDisclosure,
 } from "@chakra-ui/react";
 import { SortingViewProps } from "./types";
 import Image from "next/image";
@@ -18,7 +17,6 @@ const SortingView = ({
   label,
   option,
 }: SortingViewProps) => {
-  const { onClose, onOpen, isOpen } = useDisclosure();
   return (
     <SidebarSection className="text-custom-primary-text">
       <label className="flex gap-2 mb-4 2xl:mb-6 items-center">
@@ -35,16 +33,18 @@ const SortingView = ({
           closeOnBlur={true}
           matchWidth={true}
           gutter={10}
+          variant={"brand"}
         >
-          <MenuButton as="button" style={{ width: "100%" }}>
+          <MenuButton
+            as="button"
+            style={{ width: "100%" }}
+            className="group/menuBtn"
+          >
             <div className="w-full flex bg-custom-background px-4 py-3 border-[1px] border-custom-stroke items-center justify-between rounded-xl">
               <p className="text-sm 2xl:text-base font-bold text-custom-primary-text ">
                 {option.label}
               </p>
-              <span
-                data-is-open={isOpen}
-                className="data-[is-open=false]:rotate-180 transition-transform"
-              >
+              <span className="group-aria-[expanded=false]/menuBtn:rotate-180 transition-transform">
                 <Image src="./up_arrow.svg" width={11} height={7} alt="arrow" />
               </span>
             </div>
@@ -52,36 +52,35 @@ const SortingView = ({
           <MenuList
             w="full"
             _focusVisible={{ boxShadow: "none", outline: "none" }}
-            border="1px solid"
-            borderColor="#BFBFBF"
             borderRadius="xl"
-            py={3}
           >
-            {options.map((item, index) => (
-              <MenuItem
-                key={`${item.label}_${index}`}
-                onClick={() => {
-                  onChange(item.value);
-                }}
-                data-selected={option.value === item.value}
-                className="group"
-                p={0}
-                m={0}
-              >
-                <div className="w-full px-5 py-[6px] flex gap-2 font-medium text-sm 2xl:text-base group-data-[selected=false]:hover:bg-custom-hover-state">
-                  <Image
-                    className="group-data-[selected=false]:invisible"
-                    src="./lightning_icon_filled.svg"
-                    height={16}
-                    width={16}
-                    alt=""
-                  />
-                  <span className="group-data-[selected=true]:text-custom-accent group-data-[selected=true]:font-bold">
-                    {item.label}
-                  </span>
-                </div>
-              </MenuItem>
-            ))}
+            <div className="w-full py-3 rounded-xl bg-custom-background border-[1px] border-custom-stroke">
+              {options.map((item, index) => (
+                <MenuItem
+                  key={`${item.label}_${index}`}
+                  onClick={() => {
+                    onChange(item.value);
+                  }}
+                  data-selected={option.value === item.value}
+                  className="group"
+                  p={0}
+                  m={0}
+                >
+                  <div className="w-full px-5 py-[6px] flex gap-2 font-medium text-sm 2xl:text-base group-data-[selected=false]:hover:bg-custom-hover-state">
+                    <Image
+                      className="group-data-[selected=false]:invisible"
+                      src="./lightning_icon_filled.svg"
+                      height={16}
+                      width={16}
+                      alt=""
+                    />
+                    <span className="group-data-[selected=true]:text-custom-accent group-data-[selected=true]:font-bold">
+                      {item.label}
+                    </span>
+                  </div>
+                </MenuItem>
+              ))}
+            </div>
           </MenuList>
         </Menu>
       </FormControl>
