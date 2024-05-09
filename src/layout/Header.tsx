a1b92ad2363e7efd5991418a03774ed77bc9eb02
Emmanuel-Develops <itakpeemma@gmail.com>
2024-05-09 03:36:37 +0100

    fix: regression on num of filter

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -48,7 +48,7 @@ const Header = ({ openForm }) => {
           )}
         </button>
         {Boolean(numberOfAppliedFilters) && (
-          <div className="absolute -top-1 -right-1 rounded-full w-4 h-4 text-[11px] font-black leading-none">
+          <div className="absolute -top-1 -right-1 rounded-full w-4 h-4 text-[11px] font-black bg-custom-accent leading-none">
             <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
               {numberOfAppliedFilters}
             </span>
