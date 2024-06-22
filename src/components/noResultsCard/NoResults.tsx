c89a2c00aed6abeb3eea5c341a5c0475afc3cefc
Emmanuel-Develops <itakpeemma@gmail.com>
2024-06-22 15:24:02 +0100

    feat: return to homepage on noresult

--- src/components/noResultsCard/NoResults.tsx
+++ src/components/noResultsCard/NoResults.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@chakra-ui/react";
+import Link from "next/link";
 import React from "react";
 
 const NoResults = ({ openForm }: { openForm: () => void }) => {
@@ -6,16 +7,16 @@ const NoResults = ({ openForm }: { openForm: () => void }) => {
     <div className="no-result bg-custom-background dark:border-[1px] dark:border-custom-stroke">
       <h2 className="text-red-400 text-xl font-medium lg:mb-7">No results found</h2>
       <p className="text-custom-primary-text">You can contribute to our sources by submitting a url</p>
-      <div>
-        <Button
+      <div className="flex flex-col lg:flex-row w-fit gap-4 mx-auto mt-4">
+        <Link href="/" className="text-custom-black dark:text-white py-2 px-3 font-bold mx-auto text-sm lg:text-base bg-custom-otherLight rounded-[10px]">
+          Go back to homepage
+        </Link>
+        <button
           onClick={openForm}
-          fontWeight={400}
-          mx="auto"
-          // colorScheme="orange"
-          className="!bg-custom-accent"
+          className="flex justify-center items-center gap-2 py-2 px-3 font-bold mx-auto text-sm text-white lg:text-base bg-custom-accent disabled:bg-custom-hover-state disabled:cursor-not-allowed disabled:text-[#CCBAA3] rounded-[10px]"
         >
-          Contribute
-        </Button>
+          <span>Contribute</span>
+        </button>
       </div>
     </div>
   );
